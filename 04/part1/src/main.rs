a07f78041b48cd03778b63a568d4bf3ff82cf6d6
Ivo Murrell <ivoivo@hey.com>
2021-12-04 09:05:32 +0000

    Submission for day 4 part 1

---
+++ 04/part1/src/main.rs
@@ -0,0 +1,50 @@
+use std::fs::read_to_string;
+
+fn main() {
+    let input = read_to_string("../input").expect("failed to open input file");
+    let mut items = input.split("\n\n");
+    let balls = items
+        .next()
+        .expect("input file was empty")
+        .split(',')
+        .map(|ball| ball.parse().expect("failed to parse ball number"));
+    let mut boards: Vec<_> = items
+        .map(|board_nums| {
+            let mut board: [[Option<u32>; 5]; 5] = Default::default();
+            for (line_i, line) in board_nums.lines().enumerate() {
+                for (row_i, num) in line.split_ascii_whitespace().enumerate() {
+                    let num = num.parse().expect("failed to parse board number");
+                    board[line_i][row_i] = Some(num);
+                }
+            }
+            board
+        })
+        .collect();
+
+    for ball in balls {
+        for board in boards.iter_mut() {
+            for num in board.iter_mut().flat_map(|line| line.iter_mut()) {
+                if num == &Some(ball) {
+                    *num = None
+                }
+            }
+
+            let bingo_row = board.iter().any(|line| *line == [None; 5]);
+            let bingo_col = (0..5).any(|i| board.iter().all(|line| line[i] == None));
+            if bingo_row || bingo_col {
+                let unmarked_sum: u32 = board
+                    .iter()
+                    .flat_map(|line| line.iter())
+                    .filter_map(|num| *num)
+                    .sum();
+                println!(
+                    "The sum of all unmarked numbers on the winning board is {} and the final ball is {} so the score is {}!",
+                    unmarked_sum,
+                    ball,
+                    unmarked_sum * ball
+                );
+                return;
+            }
+        }
+    }
+}
