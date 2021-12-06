4d391ea32d1d8dabed1c2e55cdd8f7cb7a8e2458
Ivo Murrell <ivoivo@hey.com>
2021-12-06 09:07:51 +0000

    Submission for day 6 part 1

---
+++ 06/part1/src/main.rs
@@ -0,0 +1,24 @@
+use std::fs::read_to_string;
+
+fn main() {
+    let mut timers = [0u32; 9];
+    read_to_string("../input")
+        .expect("failed to open input file")
+        .lines()
+        .next()
+        .expect("input file was empty")
+        .split(',')
+        .map(|timer| timer.parse().expect("failed to parse lanternfish timer"))
+        .for_each(|fish_timer: usize| timers[fish_timer] += 1);
+
+    for _ in 0..80 {
+        timers.rotate_left(1);
+        timers[6] += timers[8];
+    }
+
+    let fish_count: u32 = timers.iter().sum();
+    println!(
+        "After 80 days there is now a total of {} lanternfish!",
+        fish_count
+    );
+}
